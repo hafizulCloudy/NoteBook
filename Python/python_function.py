eb130206a4ee4f43b54598975ef1ad976f29294c
hafizul <hafizpustice05@gmail.com>
2021-02-12 19:17:13 +0600

    up

--- Python/python_function.py
+++ Python/python_function.py
@@ -3,7 +3,7 @@
 def function():
 
     try:
-      f = open('test.txt')
+      f = open('test.txt','w+')
       content = f.write('i am a hafiz')
      
 
@@ -12,15 +12,21 @@ def function():
       print("I/O error({0}): {1}".format(errno, strerror))
 
     a = f.read()  #i am a hafiz
-    b = f.seek(0,0) # pointer is first position
+    print(a)
+    f.seek(0, 0)  # pointer is first position
+    b = f.tell()
+    print(b)
     a = f.read(2)  #i a
+    print(a)
     
     print(f.readline)  #print fist line
-    print(f.readlines) # print all line by a array
+    #lines = f.readlines # print all line by a array
       
-    for line in f:
-        print(line,end='')
+    #for line in lines:
+        #print(line)
+    f.close()
 
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     function()
